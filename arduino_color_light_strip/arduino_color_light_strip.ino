7dd21744a0f213084070fed2dd7a8ce2284ccd75
Kalle Hallivuori <kalle.hallivuori@futurice.com>
2018-04-09 19:50:11 +0300

    Flash one random led at a time. Crude first step toward stackable effects and more elegant rain drops.

--- arduino_color_light_strip/arduino_color_light_strip.ino
+++ arduino_color_light_strip/arduino_color_light_strip.ino
@@ -2,7 +2,7 @@
 
 #define COLOR_LEDS_COUNT 240
 #define COLOR_LEDS_PIN 12
-#define DELAY 50
+#define DELAY 10
 #define STRIPE_WIDTH 16
 
 CRGBArray<COLOR_LEDS_COUNT> leds;
@@ -51,9 +51,39 @@ CRGB color_for_led(uint16_t led_index, uint32_t milliseconds) {
 	uint32_t led_frame_number = frame_number + led_index_uint32; // loop over whole color palette
 	uint8_t color_index = has_led_base_color(led_index) ? 0 : led_frame_number;
 
-	return ColorFromPalette(color_palette, color_index);
+	return led_flash(led_index, milliseconds, ColorFromPalette(color_palette, color_index));
 }
 
 bool has_led_base_color(uint16_t led_index) {
 	return led_index & 7;
+}
+
+uint16_t led_flash_get_next_index() {
+	return (random() % COLOR_LEDS_COUNT);
+}
+
+uint32_t led_flash_last_time = 0, led_flash_duration = 30, led_flash_interval = 50;
+uint16_t led_flash_current_index = -1, led_flash_next_index = led_flash_get_next_index();
+CRGB led_flash_color = ColorFromPalette(color_palette, 128);
+
+CRGB led_flash(uint16_t led_index, uint32_t milliseconds, CRGB led_default_color) {
+
+	if (led_index == led_flash_current_index) {
+		if (milliseconds < led_flash_last_time + led_flash_duration) {
+			return led_flash_color;
+		}
+		led_flash_current_index = -1;
+	}
+	
+	if (led_index == led_flash_next_index) {
+		if (milliseconds > led_flash_last_time + led_flash_interval) {
+			led_flash_last_time = milliseconds;
+			led_flash_current_index = led_index;
+			led_flash_next_index = led_flash_get_next_index();
+
+			return led_flash_color;
+		}
+	} 
+
+	return led_default_color;
 }
